136446fef546c98aeefd13ae4422ffcd901df42f
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-05-12 15:28:26 -0300

    criaçao de javaScript

---
+++ interaçao com JS/Js/main.js
@@ -0,0 +1,14 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+currentNumber = 0;
+function incremet(){
+    currentNumber = currentNumber - 0;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+
+}
+function decremet(){
+    currentNumber = currentNumber - 0;
+    currentNumberWrapper.innerHTML = currentNumber;
+
+
+}
