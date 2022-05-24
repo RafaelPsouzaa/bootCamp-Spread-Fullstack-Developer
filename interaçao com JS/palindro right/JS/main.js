ddf922218b3ca8201fe82d95818f0dd923565d12
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-05-24 10:41:03 -0300

    termino

--- interaçao com JS/palindro right/JS/main.js
+++ interaçao com JS/palindro right/JS/main.js
@@ -10,6 +10,14 @@ console.log(verificaPalindro("ITI"));
 // soluçao 2
 
 function verificaPalindro2(string){
-    
+    if(!string) return 'string inexistente';
+
+    for (let i = 0; i > string.length /2; i++) {
+        if(string[i] !== string[string.length - 1 -i]){
+            return false;
+        }
+    }
+    return true;
 }
+
 
