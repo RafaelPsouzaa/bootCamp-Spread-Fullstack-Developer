4fc5f518b8fbf8b966739b4dc2937c301f50e0d7
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-05-26 10:17:58 -0300

    creation calcular idade

---
+++ interaçao com JS/calc idade/JS/main.js
@@ -0,0 +1,19 @@
+function calcularIdade(anos){
+    return`Daqui  a ${anos} anos, ${this.nome}terá ${this.idade+anos} anos de idade`;
+}
+
+const pessoa1 = {
+    nome:"Rafael",
+    idade:30,
+}
+
+const pessoa2 = {
+    nome:"clara",
+    idade: 35,
+}
+
+const animal = {
+    nome: "Fiona",
+    idade:5,
+    raca:"pastor"
+}
