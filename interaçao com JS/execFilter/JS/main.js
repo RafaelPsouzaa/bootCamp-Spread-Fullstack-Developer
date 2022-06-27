34f5cbe668789af053a4c64fd789bd2352fea0fa
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-06-27 09:36:28 -0300

    fineshed

--- interaçao com JS/execFilter/JS/main.js
+++ interaçao com JS/execFilter/JS/main.js
@@ -1,6 +1,12 @@
 function filterPar(arr){
-    return arr.filter();
+    return arr.filter(callback);
 
 }
 
-function callback(); 
+function callback(item){
+    return item % 2 === 0;
+}
+
+const myArray =[2,4,45,65,56,75,22,44,92,86];
+
+console.log(filterPar(myArray));
