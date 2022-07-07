0f3db82bd085f9f29037d1522c180105bdd0abd3
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-07-07 15:42:43 -0300

    atividade

--- interaçao com JS/exeClasses/JS/main.js
+++ interaçao com JS/exeClasses/JS/main.js
@@ -5,4 +5,20 @@ class contaBancaria {
         this.tipoCC = tipoCC;
         this._saldo = saldo;
     }
+    get saldo(){
+        return this._saldo;
+    }
+    set saldo(valor){
+       this._saldo = valor;
+    }
+    sacar (valor){
+        if(valor > this._saldo){
+            return "operaçao negada "
+        }
+        this._saldo = this._saldo - valor;
+
+        return this._saldo;
+    }
+
 }
+//04:30 video
