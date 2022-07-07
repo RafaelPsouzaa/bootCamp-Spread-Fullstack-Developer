82283b695dee100135587e6c84c9ae4534979d83
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-07-07 11:13:50 -0300

    initial classes

---
+++ interaçao com JS/exeClasses/JS/main.js
@@ -0,0 +1,8 @@
+class contaBancaria {
+    constructor(agencia,numero,tipoCC,saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipoCC = tipoCC;
+        this._saldo = saldo;
+    }
+}
