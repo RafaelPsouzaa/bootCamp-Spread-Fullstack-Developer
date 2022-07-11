b519c9bbd11239e39928c4361910194ddf2bbe62
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-07-11 11:02:21 -0300

    terminado

---
+++ interaçao com JS/exerModulo/js/funçao.mjs
@@ -0,0 +1,14 @@
+function mostraIdade (nome,idade){
+    return `A idadae ${nome} e ${idade}`;
+}
+function mostraCidade (nome,cidade){
+    return `A idadae ${nome} e ${cidade}`;
+}
+function mostraHobe (nome,hobe){
+    return `A idadae ${nome} e ${hobe}`;
+}
+export{
+    mostraCidade,
+    mostraHobe,
+    mostraIdade,
+};
