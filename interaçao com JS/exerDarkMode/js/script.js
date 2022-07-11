d9da302ad9a80dbc76b5d26c05131292ef99510d
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-07-11 15:26:38 -0300

    mudança nome

--- interaçao com JS/exerDarkMode/js/script.js
+++ interaçao com JS/exerDarkMode/js/script.js
@@ -1,6 +1,27 @@
  function changeMode(){
-    console.log('cliquei!');
+    changeClasses();
+    changeText();
  }
+ function changeClasses(){
+    button.classList.toggle(darkModeClass );
+    h1.classList.toggle(darkModeClass );
+    body.classList.toggle(darkModeClass );
+    footer.classList.toggle(darkModeClass );
+ }
+ function changeText(){
+        const lightMode = "light Mode";
+        const darkMode =  "dark Mode";
+    if(body.classList.contains(darkModeClass )){
+        button.innerHTML('lightMode');
+        h1.innerHTML(darkMode + 'on');
+        return;
+    }
+    button.innerHTML('darkMode');
+    h1.innerHTML(lightMode + 'on');
+ }
+ const darkModeClass = 'dark-mode';
  const button = document.getElementById('mode-selector');
  const h1 = document.getElementById('page-title');
+ const body = document.getElementsByTagName('body')[0];
+ const footer = document.getElementsByTagName('footer')[0];
 button.addEventListener("click",changeMode);
