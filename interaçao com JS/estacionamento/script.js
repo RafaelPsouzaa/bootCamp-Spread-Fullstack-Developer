307e9dcbb6e053400beb656ff5f84bde3e7a6203
RafaelPsouzaa <55769823+RafaelPsouzaa@users.noreply.github.com>
2022-07-18 14:24:34 -0300

    fazendo horario do estacionamento

--- interaçao com JS/estacionamento/script.js
+++ interaçao com JS/estacionamento/script.js
@@ -2,13 +2,35 @@
 (function () {
     var _a;
     const $ = (query) => document.querySelector(query);
+    function patio() {
+        function ler() {
+        }
+        function adicionar(veiculo) {
+            var _a;
+            const row = document.createElement("tr");
+            row.innerHTML = `<td>${veiculo.nome}</td>`;
+            row.innerHTML = `<td>${veiculo.placa}</td>`;
+            row.innerHTML = `<td>${veiculo.entrada}</td>`;
+            row.innerHTML = `<td><button class="delete data-placa= ${veiculo.placa}">X</button>/td>`;
+            (_a = $("#patio")) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+        }
+        function remover() {
+        }
+        function salvar() {
+        }
+        function render() {
+        }
+        return { ler, adicionar, remover, salvar, render };
+    }
     (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
         const nome = (_a = $("#nome")) === null || _a === void 0 ? void 0 : _a.value;
         const placa = (_b = $("#placa")) === null || _b === void 0 ? void 0 : _b.value;
-        if (nome || placa) {
+        console.log(nome, placa);
+        if (!nome || !placa) {
             alert(" O nome e placa sao obrigatorios");
             return;
         }
+        patio().adicionar({ nome, placa, entrada: new Date() });
     });
 })();
--- interaçao com JS/estacionamento/script.ts
+++ interaçao com JS/estacionamento/script.ts
@@ -1,14 +1,50 @@
+interface veiculo{
+    nome:string;
+    placa:string;
+    entrada:Date;
+
+
+}
+
 (function(){
-    const $ = (query:string): HTMLInputElement | null => document.querySelector(query);
+    const $ = (query:string): HTMLInputElement | null => 
+    document.querySelector(query);
+    function patio(){
+        function ler(){
+
+        }
+        function adicionar(veiculo:veiculo){
+            const row = document.createElement("tr");
+            row.innerHTML = `<td>${veiculo.nome}</td>`;
+            row.innerHTML = `<td>${veiculo.placa}</td>`;
+            row.innerHTML = `<td>${veiculo.entrada}</td>`;
+            row.innerHTML = `<td><button class="delete data-placa= ${veiculo.placa}">X</button>/td>`;
+
+            $("#patio")?.appendChild(row);
+        }
+        function remover(){
+
+        }
+        function salvar(){
+
+        }
+        function render(){
+
+        }
+
+        return {ler,adicionar,remover,salvar,render}
+    }   
+    
 
     $("#cadastrar")?.addEventListener("click",()=>{
         const nome = $("#nome")?.value;
         const placa = $("#placa")?.value;
+      
 
-        if(nome || placa){
+        if(!nome || !placa){
             alert(" O nome e placa sao obrigatorios");
             return;
         }
-
-    })
+        patio().adicionar({nome,placa,entrada: new Date()});
+    });
 })();
